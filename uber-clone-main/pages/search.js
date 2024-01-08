276c0d1950f03db11111a327101447b83ac605a8
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-08 19:24:19 +0530

    Update search.js
    Suggestions feature working now

--- uber-clone-main/pages/search.js
+++ uber-clone-main/pages/search.js
@@ -1,11 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import tw from "tailwind-styled-components";
 import Link from "next/link";
 
 function Search() {
   const [pickup, setPickup] = useState("");
   const [dropoff, setDropoff] = useState("");
+  const [searchPickUp, setSearchPickUp] = useState("");
+  const [searchDropOff, setSearchDropOff] = useState("");
+  const [autocompletePickUp, setAutocompletePickUp] = useState([]);
+  const [autocompleteDropOff, setAutocompleteDropOff] = useState([]);
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_PlacesAPI_Key,
+      "X-RapidAPI-Host": "place-autocomplete1.p.rapidapi.com",
+    },
+  };
 
+  useEffect(() => {
+    const loadAutocomplete = async () => {
+      try {
+        const url = `https://place-autocomplete1.p.rapidapi.com/autocomplete/json?input=${searchPickUp}&radius=500`;
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const queries = result.predictions.map((item) => item.description);
+        setAutocompletePickUp(queries);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    if (searchPickUp.length > 0) loadAutocomplete();
+    else setAutocompletePickUp([]);
+  }, [searchPickUp]);
+  useEffect(() => {
+    const loadAutocomplete = async () => {
+      try {
+        const url = `https://place-autocomplete1.p.rapidapi.com/autocomplete/json?input=${searchDropOff}&radius=500`;
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const queries = result.predictions.map((item) => item.description);
+        setAutocompleteDropOff(queries);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    if (searchDropOff.length > 0) loadAutocomplete();
+    else setAutocompleteDropOff([]);
+  }, [searchDropOff]);
   return (
     <Wrapper>
       <ButtonContainer>
@@ -21,17 +62,52 @@ function Search() {
         </FromToIcons>
 
         <InputBoxes>
-          <Input
-            placeholder="Enter pickup location"
-            value={pickup}
-            onChange={(e) => setPickup(e.target.value)}
-          />
-          <Input
-            placeholder="Where to?"
-            value={dropoff}
-            onChange={(e) => setDropoff(e.target.value)}
-          />
+          <ContainerBox>
+            <Input
+              placeholder="Enter pickup location"
+              value={searchPickUp}
+              onChange={(e) => setSearchPickUp(e.target.value)}
+            />
+            {autocompletePickUp.length > 0 && pickup.length < 1 && (
+              <SuggestionsList>
+                {autocompletePickUp.map((item, index) => (
+                  <SuggestionItem
+                    key={index}
+                    onClick={() => {
+                      setPickup(item);
+                      setSearchPickUp(item);
+                    }}
+                  >
+                    {item}
+                  </SuggestionItem>
+                ))}
+              </SuggestionsList>
+            )}
+          </ContainerBox>
+          <ContainerBox>
+            <Input
+              placeholder="Where to?"
+              value={searchDropOff}
+              onChange={(e) => setSearchDropOff(e.target.value)}
+            />
+            {autocompleteDropOff.length > 0 && dropoff.length < 1 && (
+              <SuggestionsList>
+                {autocompleteDropOff.map((item, index) => (
+                  <SuggestionItem
+                    key={index}
+                    onClick={() => {
+                      setDropoff(item);
+                      setSearchDropOff(item);
+                    }}
+                  >
+                    {item}
+                  </SuggestionItem>
+                ))}
+              </SuggestionsList>
+            )}
+          </ContainerBox>
         </InputBoxes>
+
         <PlusIcon src="https://img.icons8.com/ios/50/000000/plus-math.png" />
       </InputContainer>
 
@@ -96,7 +172,7 @@ const InputBoxes = tw.div`
 `;
 
 const Input = tw.input`
-    h-10 bg-gray-200 my-2 rounded-2 p-2 outline-none border-none ml-3
+    h-10 bg-gray-200 my-2 rounded-2 p-2 outline-none border-none ml-3 relative
 `;
 
 const PlusIcon = tw.img`
@@ -118,3 +194,12 @@ const ConfirmContainer = tw.div`
 const ConfirmButton = tw.div`
     text-white
 `;
+const SuggestionsList = tw.ul`
+  absolute w-2/3 mt-12 bg-white border border-gray-300 rounded-md shadow-md overflow-hidden z-10
+`;
+
+const SuggestionItem = tw.li`
+  px-4 py-2 cursor-pointer hover:bg-gray-100
+`;
+const ContainerBox = tw.div`
+flex flex-col`;
