60378e22a37b5c1086c1f7ad95c53cd0ccf19325
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-09 09:53:00 +0530

    Route feature implemented

--- uber-clone-main/pages/components/Map.js
+++ uber-clone-main/pages/components/Map.js
@@ -25,11 +25,42 @@ export const Map = (props) => {
       map.fitBounds([props.pickupCoordinates, props.dropoffCoordinates], {
         padding: 60,
       });
+      drawRoute(map, props.pickupCoordinates, props.dropoffCoordinates);
     }
   }, [props.pickupCoordinates, props.dropoffCoordinates]);
 
   const addToMap = (map, coordinates) => {
     const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  };
+  const drawRoute = async (map, start, end) => {
+    const response = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`
+    );
+    const data = await response.json();
+
+    const route = data.routes[0].geometry;
+    map.addSource("route", {
+      type: "geojson",
+      data: {
+        type: "Feature",
+        properties: {},
+        geometry: route,
+      },
+    });
+    map.addLayer({
+      id: "route",
+      type: "line",
+      source: "route",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#00FF3E",
+        "line-width": 7,
+        "line-opacity": 0.75,
+      },
+    });
   };
 
   return <Wrapper id="map"></Wrapper>;
