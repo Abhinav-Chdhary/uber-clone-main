870a74ec35744c7e42e0b56244c61bb32f428270
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-01-09 09:36:46 +0530

    Handled pop close

--- uber-clone-main/pages/login.js
+++ uber-clone-main/pages/login.js
@@ -1,48 +1,56 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { signInWithPopup, onAuthStateChanged } from 'firebase/auth';
-import { auth, provider } from '../firebase';
-import tw from 'tailwind-styled-components/dist/tailwind';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { auth, provider } from "../firebase";
+import tw from "tailwind-styled-components/dist/tailwind";
 
 function Login() {
+  const router = useRouter();
 
-    const router = useRouter();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.push("/");
+      }
+    });
+    return () => unsubscribe();
+  }, [router]);
 
-    useEffect(() => {
-        onAuthStateChanged(auth, user => {
-            if (user) {
-                router.push('/');
-            };
-        });
-    }, []);
+  const handleSignIn = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error("Error signing in:", error.message);
+    }
+  };
 
-    return (
-        <Wrapper>
-            <UberLogo src='https://i.ibb.co/ZMhy8ws/uber-logo.png' />
-            <Title>Login to access your account</Title>
-            <HeadImage src='https://i.ibb.co/CsV9RYZ/login-image.png' />
-            <SignInButton onClick={() => signInWithPopup(auth, provider)}>Sign in with Google</SignInButton>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <UberLogo src="https://i.ibb.co/ZMhy8ws/uber-logo.png" />
+      <Title>Login to access your account</Title>
+      <HeadImage src="https://i.ibb.co/CsV9RYZ/login-image.png" />
+      <SignInButton onClick={handleSignIn}>Sign in with Google</SignInButton>
+    </Wrapper>
+  );
 }
 
-export default Login
+export default Login;
 
 const Wrapper = tw.div`
     flex flex-col h-screen bg-gray-200 p-4
-`
+`;
 
 const SignInButton = tw.button`
     bg-black text-white text-center py-4 mt-8 self-center w-full
-`
+`;
 
 const UberLogo = tw.img`
     h-20 w-auto object-contain self-start
-`
+`;
 const Title = tw.div`
     text-5xl pt-4 text-gray-500
-`
+`;
 
 const HeadImage = tw.img`
     object-contain w-full
-`
+`;
